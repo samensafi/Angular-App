68a00ca5a07444312de5ddf4ec58d42dc112c426
samensafi <samensafi@yahoo.com>
2022-06-19 03:01:47 -0400

    first commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+//we turn the component to something angular will understand by adding a decorator to it @Component()
+//we create a component by creating a class
+@Component({
+  //selector allows us to use that component
+  //a basic component includes a selector and a template
+  selector: 'app-post-create',
+  templateUrl: '../post-create.component.html'
+})
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = '';
+
+//onAddPost(postInput: HTMLTextAreaElement)
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+}
