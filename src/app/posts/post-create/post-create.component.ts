6bb985f5529635c30191b016d8fb7bce0f3a2e54
samensafi <samensafi@yahoo.com>
2022-06-19 16:45:44 -0400

    Added Event Binding and Post Property

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,19 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 //we turn the component to something angular will understand by adding a decorator to it @Component()
 //we create a component by creating a class
 @Component({
   //selector allows us to use that component
   //a basic component includes a selector and a template
   selector: 'app-post-create',
-  templateUrl: '../post-create.component.html',
+  templateUrl: '../post-create/post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = '';
+  //enteredValue = '';
+  //newPost = '';
+  enteredTitle = '';
+  enteredContent = '';
+  //At first Output decorator added (imported) in line 1 and then added to below property.
+  //Used it in app.component.html to listen to postCreated
+  @Output() postCreated = new EventEmitter ();
 
 //onAddPost(postInput: HTMLTextAreaElement)
   onAddPost() {
-    this.newPost = this.enteredValue;
+    //this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    //to emit a new event and pass a post as an argument
+    this.postCreated.emit(post);
   }
 }
