00cd04863a41efa9c028673b0858ad2df99866c7
samensafi <samensafi@yahoo.com>
2022-07-12 01:38:46 -0400

    CORS

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,14 @@ const express = require('express');
 //can be used to create new middlewares
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods",
+                "GET, POST, PATCH, DELETE, OPTIONS")
+  next();
+});
 
   //doing something with the response
 app.use('/api/posts', (req, res, next) => {
