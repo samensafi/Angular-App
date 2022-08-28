e6c6db6c37c631e5acd1a0cd8032e405c0b99c72
samensafi <samensafi@yahoo.com>
2022-08-27 22:18:07 -0400

    separating mymessage from newpost

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostsService } from "./posts/post.service";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { PostsService } from "./posts/post.service";
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
