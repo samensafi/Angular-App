368ac4b68b31700e0516ab338def0610831bcdf4
Sam <samensafi@yahoo.com>
2022-12-03 15:40:30 -0500

    Updated items per page limit / default display

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -21,9 +21,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   isLoading = false;
   totalPosts = 0;
-  postsPerPage = 2;
+  postsPerPage = 5;
   currentPage = 1;
-  pageSizeOptions = [1, 2, 5, 10];
+  pageSizeOptions = [1, 2, 5, 10, 15];
   userIsAuthenticated = false;
   private postsSub: Subscription;
   private authStatusSub: Subscription;
